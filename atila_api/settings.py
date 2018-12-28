48694629f6360debed5ac5ba715fce1e89ba0edd
ademidun <tomiademidun@gmail.com>
2018-12-28 18:08:03 -0500

    refactored directory structure of atila_api directories

--- atila_api/settings.py
+++ atila_api/settings.py
@@ -22,6 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ajlfu*@1d8%@bqu=$b)iepc+r5yxit2s=anx43up1%sn7y1cgq'
 
+POSTGRES_USERNAME = os.getenv('POSTGRES_USERNAME')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -75,8 +78,13 @@ WSGI_APPLICATION = 'atila_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'atila_demo',
+        'USER': POSTGRES_USERNAME,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '',
+        'CONN_MAX_AGE':  0,
     }
 }
 
