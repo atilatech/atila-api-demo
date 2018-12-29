23164ff5f1437686d1c50bcc7ce833b5990d87fa
ademidun <tomiademidun@gmail.com>
2018-12-28 19:24:02 -0500

    added urls and views to userprofiles

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,14 +1,12 @@
 from rest_framework import routers
 
-from scholarship.views import ScholarshipViewSet
+from userprofile.views import UserProfileViewSet
 
 router = routers.DefaultRouter()
-router.register(r'', ScholarshipViewSet)
+router.register(r'', UserProfileViewSet)
 # router.register(r'applications', applications.views.ApplicationViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-# todo access urls from root api/
-# todo read about best practices for api url naming conventions
 
 urlpatterns = []
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+
+from userprofile.models import UserProfile
+from userprofile.serializers import UserProfileSerializer
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = UserProfile.objects.all()  # .order_by('-date_joined')
+    serializer_class = UserProfileSerializer
